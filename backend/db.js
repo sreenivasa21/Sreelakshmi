da228b171078e92a76fe0aec399b3c4e6ff40aeb
sreenivasa21 <srinivasarao.p230883@gmail.com>
2025-12-29 20:33:20 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,7 +1,12 @@
-const mysql = require("mysql2");
-module.exports = mysql.createConnection({
+const { Pool } = require("pg");
+
+const pool = new Pool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
-  database: process.env.DB_NAME
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT || 5432,
+  ssl: { rejectUnauthorized: false }
 });
+
+module.exports = pool;
